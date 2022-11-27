4b237d151c58a1b3bd274389947e121b37902f8d
Ingmund Olsson <ingmund@proton.me>
2022-11-28 01:54:20 +0200

    initial commit

---
+++ cmd/createbrigade/main.go
@@ -0,0 +1,364 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/base32"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"log"
+	"net/http/httputil"
+	"net/netip"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/vpngen/wordsgens/namesgenerator"
+	"github.com/vpngen/wordsgens/seedgenerator"
+	"golang.org/x/crypto/ssh"
+)
+
+const (
+	dbnameFilename       = "dbname"
+	schemaNameFilename   = "schema"
+	sshkeyFilename       = "id_ecdsa"
+	sshkeyRemoteUsername = "_valera_"
+	etcDefaultPath       = "/etc/vgdept"
+)
+
+const (
+	maxPostgresqlNameLen = 63
+	postgresqlSocket     = "/var/run/postgresql"
+)
+
+const sshTimeOut = time.Duration(5 * time.Second)
+
+const (
+	sqlPickRealm = `
+	SELECT realm_id, control_ip FROM %s LIMIT 1
+	`
+	sqlCreateBrigadier = `
+	INSERT INTO
+		%s
+		(
+			brigade_id,
+			realm_id,
+			brigadier,
+			person
+		)
+	VALUES
+		(
+			$1, $2, $3, $4
+		)
+	`
+	sqlCreateBrigadierSalt = `
+	INSERT INTO
+		%s
+		(
+			brigade_id,
+			salt
+		)
+	VALUES
+		(
+			$1, $2
+		)
+	`
+
+	sqlCreateBrigadierKey = `
+	INSERT INTO
+		%s
+		(
+			brigade_id,
+			key
+		)
+	VALUES
+		(
+			$1, $2
+		)
+	`
+
+	sqlFetchBrigadier = `
+	SELECT
+		brigadiers.brigadier,
+		brigadiers.person,
+		realms.control_ip
+	FROM
+		%s,%s
+	WHERE
+			brigadiers.brigade_id=$1
+		AND
+			realms.realm_id=brigadiers.realm_id
+	`
+)
+
+const seedPrefix = "даблять"
+
+func main() {
+	var w io.WriteCloser
+
+	confDir := os.Getenv("CONFDIR")
+	if confDir == "" {
+		confDir = etcDefaultPath
+	}
+
+	dbname, schema, err := readConfigs(confDir)
+	if err != nil {
+		log.Fatalf("Can't read configs: %s\n", err)
+	}
+
+	sshconf, err := createSSHConfig(confDir)
+	if err != nil {
+		log.Fatalf("Can't create ssh configs: %s\n", err)
+	}
+
+	db, err := createDBPool(dbname)
+	if err != nil {
+		log.Fatalf("Can't create db pool: %s\n", err)
+	}
+
+	id, mnemo, err := createBrigade(db, schema)
+	if err != nil {
+		log.Fatalf("Can't create brigade: %s\n", err)
+	}
+
+	// wgconfx = wgconf + keydesk IP
+	wgconfx, err := requestBrigade(db, schema, sshconf, id)
+	if err != nil {
+		log.Fatalf("Can't request brigade: %s\n", err)
+	}
+
+	w = httputil.NewChunkedWriter(os.Stdout)
+
+	_, err = fmt.Fprintln(w, mnemo)
+	if err != nil {
+		log.Fatalf("Can't print memo: %s\n", err)
+	}
+
+	_, err = w.Write(wgconfx)
+	if err != nil {
+		log.Fatalf("Can't print wgconfx: %s\n", err)
+	}
+}
+
+func requestBrigade(db *pgxpool.Pool, schema string, sshconf *ssh.ClientConfig, id string) ([]byte, error) {
+	ctx := context.Background()
+
+	tx, err := db.Begin(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("begin: %w", err)
+	}
+
+	var (
+		fullname   string
+		person     namesgenerator.Person
+		control_ip netip.Addr
+	)
+
+	err = tx.QueryRow(ctx,
+		fmt.Sprintf(sqlFetchBrigadier,
+			(pgx.Identifier{schema, "brigadiers"}.Sanitize()),
+			(pgx.Identifier{schema, "realms"}.Sanitize()),
+		),
+		id,
+	).Scan(
+		&fullname,
+		&person,
+		&control_ip,
+	)
+	if err != nil {
+		tx.Rollback(ctx)
+
+		return nil, fmt.Errorf("brigade query: %w", err)
+	}
+
+	err = tx.Commit(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("commit: %w", err)
+	}
+
+	cmd := fmt.Sprintf("-id %s -name %s -person %s -desc %s -url %s",
+		base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString([]byte(id)),
+		base64.StdEncoding.WithPadding(base64.StdPadding).EncodeToString([]byte(fullname)),
+		base64.StdEncoding.WithPadding(base64.StdPadding).EncodeToString([]byte(person.Name)),
+		base64.StdEncoding.WithPadding(base64.StdPadding).EncodeToString([]byte(person.Desc)),
+		base64.StdEncoding.WithPadding(base64.StdPadding).EncodeToString([]byte(person.URL)),
+	)
+
+	fmt.Fprintf(os.Stderr, "%s#%s:22 -> %s\n", sshkeyRemoteUsername, control_ip, cmd)
+
+	client, err := ssh.Dial("tcp", fmt.Sprintf("%s:22", control_ip), sshconf)
+	if err != nil {
+		return nil, fmt.Errorf("ssh dial: %w", err)
+	}
+	defer client.Close()
+
+	session, err := client.NewSession()
+	if err != nil {
+		return nil, fmt.Errorf("ssh session: %w", err)
+	}
+	defer session.Close()
+
+	var b bytes.Buffer
+
+	session.Stdout = &b
+
+	if err := session.Run(cmd); err != nil {
+		return nil, fmt.Errorf("ssh run: %w", err)
+	}
+
+	wgconfx, err := io.ReadAll(httputil.NewChunkedReader(&b))
+	if err != nil {
+		return nil, fmt.Errorf("chunk read: %w", err)
+	}
+
+	return wgconfx, nil
+}
+
+func createBrigade(db *pgxpool.Pool, schema string) (string, string, error) {
+	id := uuid.New().String()
+
+	fullname, person, err := namesgenerator.PhysicsAwardee()
+	if err != nil {
+		return "", "", fmt.Errorf("physics generate: %s", err)
+	}
+
+	mnemo, seed, salt, err := seedgenerator.Seed(seedgenerator.ENT128, seedPrefix)
+	if err != nil {
+		return "", "", fmt.Errorf("gen seed12: %w", err)
+	}
+
+	ctx := context.Background()
+
+	tx, err := db.Begin(ctx)
+	if err != nil {
+		return "", "", fmt.Errorf("begin: %w", err)
+	}
+
+	var (
+		realm_id   string
+		control_ip netip.Addr
+	)
+
+	err = tx.QueryRow(ctx, fmt.Sprintf(sqlPickRealm, (pgx.Identifier{schema, "realms"}.Sanitize()))).Scan(&realm_id, &control_ip)
+	if err != nil {
+		tx.Rollback(ctx)
+
+		return "", "", fmt.Errorf("pair query: %w", err)
+	}
+
+	_, err = tx.Exec(ctx,
+		fmt.Sprintf(sqlCreateBrigadier, (pgx.Identifier{schema, "brigadiers"}.Sanitize())),
+		id,
+		realm_id,
+		fullname,
+		person,
+	)
+	if err != nil {
+		tx.Rollback(ctx)
+
+		return "", "", fmt.Errorf("create brigadier: %w", err)
+	}
+
+	_, err = tx.Exec(ctx,
+		fmt.Sprintf(sqlCreateBrigadierSalt, (pgx.Identifier{schema, "brigadier_salts"}.Sanitize())),
+		id,
+		salt,
+	)
+	if err != nil {
+		tx.Rollback(ctx)
+
+		return "", "", fmt.Errorf("create brigadier salt: %w", err)
+	}
+
+	_, err = tx.Exec(ctx,
+		fmt.Sprintf(sqlCreateBrigadierKey, (pgx.Identifier{schema, "brigadier_keys"}.Sanitize())),
+		id,
+		seed,
+	)
+	if err != nil {
+		tx.Rollback(ctx)
+
+		return "", "", fmt.Errorf("create brigadier key: %w", err)
+	}
+
+	err = tx.Commit(ctx)
+	if err != nil {
+		return "", "", fmt.Errorf("commit: %w", err)
+	}
+
+	return id, mnemo, nil
+}
+
+/*brigadierID := base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(id[:])
+brigadierName := base64.StdEncoding.WithPadding(base64.StdPadding).EncodeToString([]byte(fullname))
+personName := base64.StdEncoding.WithPadding(base64.StdPadding).EncodeToString([]byte(person.Name))
+personDesc := base64.StdEncoding.WithPadding(base64.StdPadding).EncodeToString([]byte(person.Desc))
+personURL := base64.StdEncoding.WithPadding(base64.StdPadding).EncodeToString([]byte(person.URL))*/
+
+func createDBPool(dbname string) (*pgxpool.Pool, error) {
+	config, err := pgxpool.ParseConfig(fmt.Sprintf("host=%s dbname=%s", postgresqlSocket, dbname))
+	if err != nil {
+		return nil, fmt.Errorf("conn string: %w", err)
+	}
+
+	pool, err := pgxpool.NewWithConfig(context.Background(), config)
+	if err != nil {
+		return nil, fmt.Errorf("create pool: %w", err)
+	}
+
+	return pool, nil
+}
+
+func readConfigs(path string) (string, string, error) {
+	f, err := os.Open(filepath.Join(path, dbnameFilename))
+	if err != nil {
+		return "", "", fmt.Errorf("can't open: %s: %w", dbnameFilename, err)
+	}
+
+	dbname, err := io.ReadAll(io.LimitReader(f, maxPostgresqlNameLen))
+	if err != nil {
+		return "", "", fmt.Errorf("can't read: %s: %w", dbnameFilename, err)
+	}
+
+	f, err = os.Open(filepath.Join(path, schemaNameFilename))
+	if err != nil {
+		return "", "", fmt.Errorf("can't open: %s: %w", schemaNameFilename, err)
+	}
+
+	schema, err := io.ReadAll(io.LimitReader(f, maxPostgresqlNameLen))
+	if err != nil {
+		return "", "", fmt.Errorf("can't read: %s: %w", schemaNameFilename, err)
+	}
+
+	return string(dbname), string(schema), nil
+}
+
+func createSSHConfig(path string) (*ssh.ClientConfig, error) {
+	// var hostKey ssh.PublicKey
+
+	key, err := os.ReadFile(filepath.Join(path, sshkeyFilename))
+	if err != nil {
+		return nil, fmt.Errorf("read private key: %w", err)
+	}
+
+	signer, err := ssh.ParsePrivateKey(key)
+	if err != nil {
+		return nil, fmt.Errorf("parse private key: %w", err)
+	}
+
+	config := &ssh.ClientConfig{
+		User: sshkeyRemoteUsername,
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		},
+		// HostKeyCallback: ssh.FixedHostKey(hostKey),
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		Timeout:         sshTimeOut,
+	}
+
+	return config, nil
+}
---
+++ install/000-install.sql
@@ -0,0 +1,48 @@
+BEGIN;
+
+CREATE SCHEMA :"schema_name";
+
+-- Realms.
+CREATE TABLE :"schema_name".realms (
+    realm_id            uuid PRIMARY KEY NOT NULL,
+    control_ip          inet UNIQUE NOT NULL,
+    is_active           bool NOT NULL
+);
+
+CREATE TABLE :"schema_name".brigadiers (
+    brigade_id          uuid PRIMARY KEY NOT NULL,
+    realm_id            uuid NOT NULL,
+    brigadier           text UNIQUE NOT NULL,
+    person              json NOT NULL,
+    FOREIGN KEY (realm_id) REFERENCES :"schema_name".realms (realm_id)
+);
+
+CREATE TABLE :"schema_name".brigadier_salts (
+    brigade_id          uuid NOT NULL REFERENCES :"schema_name".brigadiers (brigade_id),
+    salt                bytea
+);
+
+CREATE TABLE :"schema_name".brigadier_keys (
+    brigade_id          uuid NOT NULL REFERENCES :"schema_name".brigadiers (brigade_id),
+    key                 bytea
+);
+
+CREATE TABLE :"schema_name".brigadiers_queue (
+    queue_id serial PRIMARY KEY,
+    payload json NOT NULL,
+    error json
+);
+
+CREATE ROLE :"realms_dbuser" WITH LOGIN;
+GRANT USAGE ON SCHEMA :"schema_name" TO :"realms_dbuser";
+GRANT SELECT,INSERT,UPDATE,DELETE ON :"schema_name".realms TO :"realms_dbuser";
+GRANT SELECT ON ALL TABLES IN SCHEMA :"schema_name" TO :"realms_dbuser";
+GRANT USAGE,SELECT ON ALL SEQUENCES IN SCHEMA :"schema_name"  TO :"realms_dbuser";
+
+CREATE ROLE :"brigadiers_dbuser" WITH LOGIN;
+GRANT USAGE ON SCHEMA :"schema_name" TO :"brigadiers_dbuser";
+GRANT SELECT ON :"schema_name".realms TO :"brigadiers_dbuser";
+GRANT SELECT,UPDATE,INSERT,DELETE ON :"schema_name".brigadiers, :"schema_name".brigadier_salts, :"schema_name".brigadier_keys TO :"brigadiers_dbuser";
+GRANT USAGE,SELECT,UPDATE ON SEQUENCE :"schema_name".brigadiers_queue_queue_id_seq TO :"brigadiers_dbuser";
+
+COMMIT;

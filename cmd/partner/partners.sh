fdcb569daa9f62de21355094cda22117a08e8805
Ingmund Olsson <ingmund@proton.me>
2023-05-23 16:07:11 +0300

    fix: fix

--- cmd/partner/partners.sh
+++ cmd/partner/partners.sh
@@ -80,7 +80,7 @@ infopartner () {
 BEGIN;
         SELECT 'Partner :' AS head, * FROM :"schema_name".partners WHERE partner_id=:'partner_id';
         SELECT CONCAT('    token: ',translate(encode(token, 'base64'),'+/=','-_'), ':', name) FROM :"schema_name".partners_tokens WHERE partner_id=:'partner_id';
-        SELECT CONCAT('    realm: ', r.realm_id, r.realm_name, r.is_active, r.free_slots) FROM :"schema_name".partners_realms pr JOIN :"schema_name".realms r ON pr.realm_id=r.realm_id WHERE partner_id=:'partner_id';
+        SELECT CONCAT('    realm: ', r.realm_id, '  name: ', r.realm_name, '  active: ', r.is_active, '  slots: ',r.free_slots) FROM :"schema_name".partners_realms pr JOIN :"schema_name".realms r ON pr.realm_id=r.realm_id WHERE partner_id=:'partner_id';
 COMMIT;
 EOF
 }
